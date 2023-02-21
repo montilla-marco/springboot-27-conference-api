ce0882151b825d3a0196c989e25b49a2968ac788
Marco Montilla <montilla.marcoa@gmail.com>
2023-02-21 05:14:45 -0300

    fix(web) serialization issue in JPA relationships
    JPA lazy and eager initilization is affected by Jackson Serialization
    
    To avoid this wrong behavior it must to put  @JsonIgnore in the weak side of relation

--- src/main/java/springboot/intro/conference/entity/Speaker.java
+++ src/main/java/springboot/intro/conference/entity/Speaker.java
@@ -1,5 +1,7 @@
 package springboot.intro.conference.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -34,6 +36,7 @@ public class Speaker {
     private byte[] speakerPhoto;
 
     @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
     private List<Session> sessions;
 
     public Speaker() {
